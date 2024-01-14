c6a302e99caa25790e3323fd63e323e394dfda61
hegunhee <gunhee0072@gmail.com>
2024-01-14 15:41:17 +0900

    [build-setting] feat : hilt App 설정

---
+++ app/src/main/java/com/hegunhee/maplefinder/maplefinder/MapleFinderApplication.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.maplefinder.maplefinder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MapleFinderApplication : Application() {
+}
