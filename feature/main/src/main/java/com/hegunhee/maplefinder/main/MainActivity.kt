e5a81ac89072064096a214df0698c339c61197be
hegunhee <gunhee0072@gmail.com>
2024-01-14 15:54:25 +0900

    [build-setting] style : MainActivity 앱모듈에서 feature:main 모듈로 이전

--- feature/main/src/main/java/com/hegunhee/maplefinder/main/MainActivity.kt
+++ feature/main/src/main/java/com/hegunhee/maplefinder/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hegunhee.maplefinder.maplefinder
+package com.hegunhee.maplefinder.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hegunhee.maplefinder.maplefinder.ui.theme.MapleFinderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
