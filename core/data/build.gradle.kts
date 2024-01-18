13c7d6fad04ca12abb4ec6af3b5cdcdf90dcb2a0
hegunhee <gunhee0072@gmail.com>
2024-01-18 15:37:31 +0900

    [api-setting] feat : data모듈 네트워크관련 의존성 추가, api-key properties 정의

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,12 +1,32 @@
 plugins {
     id("maplefinder.android.library")
     id("maplefinder.android.hilt")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
     namespace = "com.hegunhee.maplefinder.data"
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
 
+    implementation(project(":core:domain"))
+    implementation(project(":core:model"))
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.bundles.retrofit2)
+    implementation(libs.bundles.moshi)
+}
+
+secrets {
+    defaultPropertiesFileName = "secrets.defaults.properties"
 }
