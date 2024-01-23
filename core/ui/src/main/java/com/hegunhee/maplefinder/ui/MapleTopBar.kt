260049bd25691cf33a2848a7340b765ba0e4ee57
hegunhee <gunhee0072@gmail.com>
2024-01-23 21:15:22 +0900

    [navigation-drawer] feat : 모든 화면의 Scaffold TopBar 작성

---
+++ core/ui/src/main/java/com/hegunhee/maplefinder/ui/MapleTopBar.kt
@@ -0,0 +1,26 @@
+package com.hegunhee.maplefinder.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MapleTopBar(
+    title : String,
+    onNavigationIconClick : () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+            }
+        }
+    )
+}
