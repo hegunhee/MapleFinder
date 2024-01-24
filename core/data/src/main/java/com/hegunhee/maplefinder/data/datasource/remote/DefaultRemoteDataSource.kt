46d172b47c121cda04ec2bc951c53b0603849e57
hegunhee <gunhee0072@gmail.com>
2024-01-24 18:33:41 +0900

    [dojang-search] feat : 캐틱터의 무릉도장 정보 model 클래스, 정보를 가져오는 도메인 코드 작성

--- core/data/src/main/java/com/hegunhee/maplefinder/data/datasource/remote/DefaultRemoteDataSource.kt
+++ core/data/src/main/java/com/hegunhee/maplefinder/data/datasource/remote/DefaultRemoteDataSource.kt
@@ -2,10 +2,20 @@ package com.hegunhee.maplefinder.data.datasource.remote
 
 import com.hegunhee.maplefinder.data.api.MapleCharacterApi
 import com.hegunhee.maplefinder.data.api.MapleOcidApi
+import com.hegunhee.maplefinder.data.api.model.CharacterDojangResponse
+import com.hegunhee.maplefinder.data.api.model.OcidResponse
 import javax.inject.Inject
 
 class DefaultRemoteDataSource @Inject constructor(
     private val mapleOcidApi: MapleOcidApi,
     private val mapleCharacterApi: MapleCharacterApi
 ) : RemoteDataSource {
+
+    override suspend fun getCharacterOcid(characterName: String): OcidResponse {
+        return mapleOcidApi.getOcid(characterName)
+    }
+
+    override suspend fun getCharacterDojang(ocid: String, date: String): CharacterDojangResponse {
+        return mapleCharacterApi.getCharacterDojang(ocid,date)
+    }
 }
--- core/data/src/main/java/com/hegunhee/maplefinder/data/repository/DefaultRepository.kt
+++ core/data/src/main/java/com/hegunhee/maplefinder/data/repository/DefaultRepository.kt
@@ -1,7 +1,22 @@
 package com.hegunhee.maplefinder.data.repository
 
+import com.hegunhee.maplefinder.data.datasource.remote.RemoteDataSource
+import com.hegunhee.maplefinder.data.mapper.toCharacterDojang
 import com.hegunhee.maplefinder.domain.repository.Repository
+import com.hegunhee.maplefinder.model.character.CharacterDojang
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
-class DefaultRepository @Inject constructor() : Repository {
+class DefaultRepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : Repository {
+
+    override suspend fun getCharacterDojangInfo(characterName: String,date : String): Result<CharacterDojang> = coroutineScope {
+        runCatching {
+            val ocid = remoteDataSource.getCharacterOcid(characterName).id
+            val characterDojangResponse = remoteDataSource.getCharacterDojang(ocid,date).toCharacterDojang(characterName)
+            characterDojangResponse
+        }
+    }
+
 }
