1650d300d485184597d676f75b3a23d208c334aa
hegunhee <gunhee0072@gmail.com>
2024-01-24 18:45:16 +0900

    [dojang-search] feat : 무릉도장 데이터가 ui에 잘 표시되는지 확인

--- feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
+++ feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
@@ -7,21 +7,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.hegunhee.maplefinder.model.character.CharacterDojang
 import com.hegunhee.maplefinder.ui.MapleTopBar
 
 @Composable
 fun DojangScreenRoot(
-    onNavigationIconClick : () -> Unit
+    viewModel : DojangViewModel = hiltViewModel(),
+    onNavigationIconClick : () -> Unit,
 ) {
+    val uiState = viewModel.characterDojang.collectAsStateWithLifecycle().value
     DojangScreen(
-        onNavigationIconClick = onNavigationIconClick
+        onNavigationIconClick = onNavigationIconClick,
+        uiState
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun DojangScreen(
-    onNavigationIconClick : () -> Unit
+    onNavigationIconClick : () -> Unit,
+    characterDojang : CharacterDojang
 ) {
     Scaffold(
         topBar = { MapleTopBar(
@@ -33,6 +40,7 @@ private fun DojangScreen(
             modifier = Modifier.padding(paddingValues)
         ) {
             Text("무릉도장 화면입니다.")
+            Text(characterDojang.toString())
         }
     }
 }
