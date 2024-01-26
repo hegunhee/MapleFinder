5c1d3f122960a8651babd6c11cd85e1595cbb4a0
hegunhee <gunhee0072@gmail.com>
2024-01-26 19:26:57 +0900

    [dojang-search] feat : 무릉도장 검색화면에 무릉도장 정보 화면 나오게 설정

--- feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
+++ feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
@@ -1,35 +1,63 @@
 package com.hegunhee.maplefinder.dojang_record
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hegunhee.maplefinder.model.character.CharacterDojang
 import com.hegunhee.maplefinder.ui.MapleTopBar
+import com.hegunhee.maplefinder.ui.surface.DojangSurface
 
 @Composable
 fun DojangScreenRoot(
     viewModel : DojangViewModel = hiltViewModel(),
     onNavigationIconClick : () -> Unit,
 ) {
-    val uiState = viewModel.characterDojang.collectAsStateWithLifecycle().value
-    DojangScreen(
-        onNavigationIconClick = onNavigationIconClick,
-        uiState
-    )
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
+    val characterQuery = viewModel.searchQuery.collectAsStateWithLifecycle().value
+    when(uiState) {
+        DojangUiState.Loading -> { }
+        is DojangUiState.Search -> {
+            DojangScreen(
+                characterQuery = characterQuery,
+                characterDojang = uiState.characterDojang,
+                onNavigationIconClick = onNavigationIconClick,
+                onSearchCharacterDojang = viewModel::getCharacterDojang,
+                onQueryChange = viewModel::onQueryChange
+            )
+        }
+        DojangUiState.Error -> { }
+    }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 private fun DojangScreen(
+    characterQuery : String,
+    characterDojang : CharacterDojang?,
     onNavigationIconClick : () -> Unit,
-    characterDojang : CharacterDojang
+    onSearchCharacterDojang : (String) -> Unit,
+    onQueryChange : (String) -> Unit
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     Scaffold(
         topBar = { MapleTopBar(
             title = "무릉도장 검색",
@@ -37,10 +65,31 @@ private fun DojangScreen(
         ) }
     ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(10.dp)
         ) {
-            Text("무릉도장 화면입니다.")
-            Text(characterDojang.toString())
+            // 날짜 선택칸
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = characterQuery,
+                onValueChange = onQueryChange,
+                label = { Text("캐릭터 이름을 입력해주세요")},
+                singleLine = true,
+                maxLines = 1,
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = null, tint = Color.Black)
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {
+                    onSearchCharacterDojang(characterQuery)
+                    keyboardController?.hide()
+                }),
+            )
+            Spacer(modifier = Modifier.padding(vertical = 10.dp))
+            if(characterDojang != null) {
+                DojangSurface(characterDojang = characterDojang)
+            }
         }
     }
 }
