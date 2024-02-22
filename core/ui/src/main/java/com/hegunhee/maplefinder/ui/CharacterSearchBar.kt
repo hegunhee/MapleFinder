66b08b5c17a4b87ac4a6fce327a8b0a26f16af52
hegunhee <gunhee0072@gmail.com>
2024-02-22 14:58:24 +0900

    [character-info-api] feat : 캐릭터 검색창을 하나의 컴포저블 함수로 분리

---
+++ core/ui/src/main/java/com/hegunhee/maplefinder/ui/CharacterSearchBar.kt
@@ -0,0 +1,47 @@
+package com.hegunhee.maplefinder.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun CharacterSearchBar(
+    searchQuery : String,
+    onSearchCharacterClick : (String) -> Unit,
+    onQueryChange : (String) -> Unit,
+    keyboardController: SoftwareKeyboardController?
+) {
+    Row {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = searchQuery,
+            onValueChange = onQueryChange,
+            label = { Text("캐릭터 이름을 입력해주세요") },
+            singleLine = true,
+            maxLines = 1,
+            trailingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null, tint = Color.Black)
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(onSearch = {
+                onSearchCharacterClick(searchQuery)
+                keyboardController?.hide()
+            }),
+        )
+    }
+
+}
--- feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
+++ feature/dojang-record/src/main/java/com/hegunhee/maplefinder/dojang_record/DojangScreenRoot.kt
@@ -2,27 +2,18 @@ package com.hegunhee.maplefinder.dojang_record
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hegunhee.maplefinder.model.character.CharacterDojang
+import com.hegunhee.maplefinder.ui.CharacterSearchBar
 import com.hegunhee.maplefinder.ui.MapleTopBar
 import com.hegunhee.maplefinder.ui.surface.DojangSurface
 
@@ -69,22 +60,11 @@ private fun DojangScreen(
                 .padding(paddingValues)
                 .padding(10.dp)
         ) {
-            // 날짜 선택칸
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = characterQuery,
-                onValueChange = onQueryChange,
-                label = { Text("캐릭터 이름을 입력해주세요")},
-                singleLine = true,
-                maxLines = 1,
-                trailingIcon = {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = null, tint = Color.Black)
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
-                keyboardActions = KeyboardActions(onSearch = {
-                    onSearchCharacterDojang(characterQuery)
-                    keyboardController?.hide()
-                }),
+            CharacterSearchBar(
+                searchQuery = characterQuery,
+                onSearchCharacterClick = onSearchCharacterDojang,
+                onQueryChange = onQueryChange,
+                keyboardController = keyboardController
             )
             Spacer(modifier = Modifier.padding(vertical = 10.dp))
             if(characterDojang != null) {
