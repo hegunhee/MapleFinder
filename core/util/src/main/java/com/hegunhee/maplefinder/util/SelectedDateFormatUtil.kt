4c361a0e5a275427cd8a005b5d9e2731e667eafc
hegunhee <gunhee0072@gmail.com>
2024-03-10 11:21:57 +0900

    [date-picker] feat : 정보 조회할때 가장 최근의 정보를 조회하도록 설정

--- core/util/src/main/java/com/hegunhee/maplefinder/util/SelectedDateFormatUtil.kt
+++ core/util/src/main/java/com/hegunhee/maplefinder/util/SelectedDateFormatUtil.kt
@@ -1,5 +1,9 @@
 package com.hegunhee.maplefinder.util
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 object SelectedDateFormatUtil {
 
     fun dateToApiFormat(year : Int, month : Int, dayOfMonth : Int) : String {
@@ -7,4 +11,17 @@ object SelectedDateFormatUtil {
         val formatDayOfMonth = String.format("%02d",dayOfMonth)
         return "${year}-${formatMonth}-${formatDayOfMonth}"
     }
+
+    // 오전 1시 이후로 어제의 데이터 조회가 가능함
+    fun defaultDate() : String{
+        val currentDate = LocalDateTime.now().minusDays(1)
+        val currentHour = currentDate.hour
+        return if(currentHour > 1) {
+            currentDate.format(DateFormat)
+        }else {
+            currentDate.minusDays(1).format(DateFormat)
+        }
+    }
+
+    private val DateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 }
