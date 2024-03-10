a4cddf83560642873c01e145a29dc8b08a8e6c88
hegunhee <gunhee0072@gmail.com>
2024-03-10 13:46:54 +0900

    [date-picker] feat : 검색창에 날짜도 변경 가능하도록 설정

--- core/ui/src/main/java/com/hegunhee/maplefinder/ui/CharacterSearchBar.kt
+++ core/ui/src/main/java/com/hegunhee/maplefinder/ui/CharacterSearchBar.kt
@@ -1,47 +1,61 @@
 package com.hegunhee.maplefinder.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun CharacterSearchBar(
     searchQuery : String,
+    date : String = "",
     onSearchCharacterClick : (String) -> Unit,
     onQueryChange : (String) -> Unit,
+    onDatePickerShowClick : () -> Unit = {},
     keyboardController: SoftwareKeyboardController?
 ) {
-    Row {
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = searchQuery,
-            onValueChange = onQueryChange,
-            label = { Text("캐릭터 이름을 입력해주세요") },
-            singleLine = true,
-            maxLines = 1,
-            trailingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = null, tint = Color.Black)
-            },
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
-            keyboardActions = KeyboardActions(onSearch = {
-                onSearchCharacterClick(searchQuery)
-                keyboardController?.hide()
-            }),
-        )
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "선택된 날짜 : $date")
+            IconButton(onClick = { onDatePickerShowClick() }) {
+                Icon(imageVector = Icons.Default.DateRange, contentDescription ="selectDate")
+            }
+        }
+        Row {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = searchQuery,
+                onValueChange = onQueryChange,
+                label = { Text("캐릭터 이름을 입력해주세요") },
+                singleLine = true,
+                maxLines = 1,
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = null, tint = Color.Black)
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {
+                    onSearchCharacterClick(searchQuery)
+                    keyboardController?.hide()
+                }),
+            )
+        }
     }
+
 
 }
