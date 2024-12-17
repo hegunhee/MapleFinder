6a4c2ae285484a6f03dcc8f5d266fc614f1dbb48
이건희 <leech9876@naver.com>
2024-12-17 16:37:11 +0900

    #19 - feat : CommonExtension 매개변수 변경
    AGP 버전 변경에 따른 CommonExtension 스펙의 변경이 있어서 매개변수를 변경했습니다.

--- build-logic/src/main/java/com/hegunhee/maplemfinder/build_logic/setup/AndroidGradleDsl.kt
+++ build-logic/src/main/java/com/hegunhee/maplemfinder/build_logic/setup/AndroidGradleDsl.kt
@@ -9,13 +9,13 @@ import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.api.Project
 
-internal val Project.applicationExtension: CommonExtension<*, *, *, *>
+internal val Project.applicationExtension: CommonExtension<*, *, *, *, *, *>
     get() = extensions.getByType<ApplicationExtension>()
 
-internal val Project.libraryExtension: CommonExtension<*, *, *, *>
+internal val Project.libraryExtension: CommonExtension<*, *, *, *,*,*>
     get() = extensions.getByType<LibraryExtension>()
 
-internal val Project.androidExtension: CommonExtension<*, *, *, *>
+internal val Project.androidExtension: CommonExtension<*, *, *, *,*,*>
     get() = runCatching { libraryExtension }
         .recoverCatching { applicationExtension }
         .onFailure { println("Could not find Library or Application extension from this project") }
