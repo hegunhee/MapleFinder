b12c81fafd3d37336470c694efb5212b41c5a953
이건희 <leech9876@naver.com>
2024-12-20 22:49:52 +0900

    #36 - test : 기본 정보를 호출하는 api 테스트코드 작성

--- core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterBasicUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterBasicUnitTest.kt
@@ -1,21 +1,22 @@
 package com.hegunhee.maplefinder.data.api.character
 
-import com.hegunhee.maplefinder.data.api.TestParameter
 import com.hegunhee.maplefinder.data.api.MapleCharacterApi
 import com.hegunhee.maplefinder.data.api.MapleOcidApi
+import com.hegunhee.maplefinder.data.api.TestParameter.CHARACTER_NAME
+import com.hegunhee.maplefinder.data.api.TestParameter.INQUIRY_DATE
 import com.hegunhee.maplefinder.data.di.ApiModule.provideConverterFactory
 import com.hegunhee.maplefinder.data.di.ApiModule.provideJson
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleApi
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleOcidApi
-import com.hegunhee.maplefinder.data.mapper.toModel
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
 class GetCharacterBasicUnitTest {
 
     private lateinit var mapleOcidApi: MapleOcidApi
-    private lateinit var mapleCharacterApi : MapleCharacterApi
+    private lateinit var mapleCharacterApi: MapleCharacterApi
 
     @Before
     fun initApi() {
@@ -27,50 +28,17 @@ class GetCharacterBasicUnitTest {
     // 테스트 결과 캐릭터명 = 엔젤릭버스터
     // 새로운 규칙 : 2023-12-22 이후 ~ 어제의 날짜까지 조회 가능함
     @Test
-    fun `get character basic info test`() {
+    fun givenCharacterName_whenGetBasic_thenReturnBasicInfos() {
         runBlocking {
-            runCatching {
-                val ocid = mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME).id
-                mapleCharacterApi.getCharacterBasic(
-                    ocid = ocid,
-                    date = "2024-02-20"
-                )
-            }.onSuccess { characterBasic ->
-                if(characterBasic.jobName == "엔젤릭버스터") {
-                    println(characterBasic.toString())
-                    assert(true)
-                }else {
-                    println(characterBasic.toString())
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
-            }
+            // Given
+            val ocid = mapleOcidApi.getOcid(characterName = CHARACTER_NAME).id
+
+            // When
+            val basicInfo = mapleCharacterApi.getCharacterBasic(ocid = ocid, date = INQUIRY_DATE)
+
+            // Then
+            Assert.assertEquals(basicInfo.jobName, "엔젤릭버스터")
         }
     }
 
-    @Test
-    fun `get character basic model test`() {
-        runBlocking {
-            runCatching {
-                val ocid = mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME).id
-                mapleCharacterApi.getCharacterBasic(
-                    ocid = ocid,
-                    date = "2024-02-20"
-                ).toModel()
-            }.onSuccess { characterBasic ->
-                if(characterBasic.jobName == "엔젤릭버스터") {
-                    println(characterBasic.toString())
-                    assert(true)
-                }else {
-                    println(characterBasic.toString())
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
-            }
-        }
-    }
 }
