9918bc655c22a1dd2b4efa51fd11fa5d72b72f56
이건희 <leech9876@naver.com>
2024-12-20 22:53:52 +0900

    #36 - test : 도장 정보 api 테스트 작성

--- core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterDojangUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterDojangUnitTest.kt
@@ -1,20 +1,22 @@
 package com.hegunhee.maplefinder.data.api.character
 
-import com.hegunhee.maplefinder.data.api.TestParameter
 import com.hegunhee.maplefinder.data.api.MapleCharacterApi
 import com.hegunhee.maplefinder.data.api.MapleOcidApi
+import com.hegunhee.maplefinder.data.api.TestParameter.CHARACTER_NAME
+import com.hegunhee.maplefinder.data.api.TestParameter.INQUIRY_DATE
 import com.hegunhee.maplefinder.data.di.ApiModule.provideConverterFactory
 import com.hegunhee.maplefinder.data.di.ApiModule.provideJson
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleApi
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleOcidApi
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
 class GetCharacterDojangUnitTest {
 
     private lateinit var mapleOcidApi: MapleOcidApi
-    private lateinit var mapleCharacterApi : MapleCharacterApi
+    private lateinit var mapleCharacterApi: MapleCharacterApi
 
     @Before
     fun initApi() {
@@ -26,26 +28,17 @@ class GetCharacterDojangUnitTest {
     // 테스트 결과 캐릭터명 = 엔젤릭버스터
     // 새로운 규칙 : 2023-12-22 이후 ~ 어제의 날짜까지 조회 가능함
     @Test
-    fun `get Character Dojang Test`() {
+    fun givenCharacterName_whenGetDojangInfo_then() {
         runBlocking {
-            runCatching {
-                val ocid = mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME).id
-                mapleCharacterApi.getCharacterDojang(
-                    ocid = ocid,
-                    date = "2024-01-22"
-                )
-            }.onSuccess { characterDojang ->
-                if(characterDojang.characterClass == "엔젤릭버스터") {
-                    println(characterDojang.toString())
-                    assert(true)
-                }else {
-                    println(characterDojang.toString())
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
-            }
+            // Given
+            val ocid = mapleOcidApi.getOcid(characterName = CHARACTER_NAME).id
+
+            // When
+            val dojang = mapleCharacterApi.getCharacterDojang(ocid = ocid, date = INQUIRY_DATE)
+
+            // Then
+            Assert.assertEquals(dojang.characterClass, "엔젤릭버스터")
         }
     }
+
 }
