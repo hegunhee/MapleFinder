0b9dbb48e2defdb6354015251683a818249f059b
이건희 <leech9876@naver.com>
2024-12-20 22:27:13 +0900

    #36 - test : 단위테스트 메서드 명 수정, 에러가 발생했을때 케이스도 추가

--- core/data/src/test/java/com/hegunhee/maplefinder/data/api/ocid/GetOcidUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplefinder/data/api/ocid/GetOcidUnitTest.kt
@@ -2,10 +2,15 @@ package com.hegunhee.maplefinder.data.api.ocid
 
 import com.hegunhee.maplefinder.data.api.TestParameter
 import com.hegunhee.maplefinder.data.api.MapleOcidApi
+import com.hegunhee.maplefinder.data.api.TestParameter.CHARACTER_NAME
+import com.hegunhee.maplefinder.data.api.TestParameter.CHARACTER_OCID
+import com.hegunhee.maplefinder.data.api.createResult
 import com.hegunhee.maplefinder.data.di.ApiModule.provideConverterFactory
 import com.hegunhee.maplefinder.data.di.ApiModule.provideJson
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleOcidApi
+import com.hegunhee.maplefinder.model.exception.NexonApiException
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -21,21 +26,32 @@ class GetOcidUnitTest {
     // 테스트 닉네임 = 엔버는함초롱
     // 테스트 결과 =05a9d4c40c549bae73b529491cce27d5
     @Test
-    fun `get Ocid Test`() {
+    fun givenCharacterName_whenGetOcid_thenReturnOcid() {
         runBlocking {
-            runCatching {
-                mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME)
-            }.onSuccess {
-                if(it.id == TestParameter.CHARACTER_OCID) {
-                    println(it.id)
-                    assert(true)
-                }else {
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
+            // Given
+
+            // When
+            val ocid = mapleOcidApi.getOcid(characterName = CHARACTER_NAME).id
+
+            // Then
+            assertEquals(ocid, CHARACTER_OCID)
+        }
+    }
+
+    @Test
+    fun givenEmptyName_whenGetOcid_thenReturn400_INVAILED_PARAMETER() {
+        runBlocking {
+            createResult {
+                // Given
+                val emptyCharacterName = ""
+
+                // When
+                mapleOcidApi.getOcid(emptyCharacterName)
+            }.onFailure { exception ->
+                // Then
+                assertEquals(exception, NexonApiException.InvailedParameterException)
             }
         }
     }
+
 }
