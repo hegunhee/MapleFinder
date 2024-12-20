c29ef65ab1b9a55f29846fccc3e3f3c205014be9
이건희 <leech9876@naver.com>
2024-12-20 22:56:39 +0900

    #36 - test : 하이퍼스텟 api 테스트 코드 작성

--- core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterHyperStatUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterHyperStatUnitTest.kt
@@ -1,21 +1,22 @@
 package com.hegunhee.maplefinder.data.api.character
 
-import com.hegunhee.maplefinder.data.api.TestParameter
 import com.hegunhee.maplefinder.data.api.MapleCharacterApi
 import com.hegunhee.maplefinder.data.api.MapleOcidApi
+import com.hegunhee.maplefinder.data.api.TestParameter.CHARACTER_NAME
+import com.hegunhee.maplefinder.data.api.TestParameter.INQUIRY_DATE
 import com.hegunhee.maplefinder.data.di.ApiModule.provideConverterFactory
 import com.hegunhee.maplefinder.data.di.ApiModule.provideJson
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleApi
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleOcidApi
-import com.hegunhee.maplefinder.data.mapper.toModel
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
 class GetCharacterHyperStatUnitTest {
 
     private lateinit var mapleOcidApi: MapleOcidApi
-    private lateinit var mapleCharacterApi : MapleCharacterApi
+    private lateinit var mapleCharacterApi: MapleCharacterApi
 
     @Before
     fun initApi() {
@@ -27,50 +28,17 @@ class GetCharacterHyperStatUnitTest {
     // 테스트 결과 캐릭터명 = 엔젤릭버스터
     // 새로운 규칙 : 2023-12-22 이후 ~ 어제의 날짜까지 조회 가능함
     @Test
-    fun `get hyper stat info test`() {
+    fun givenCharacterOcid_whenGetHyperStat_thenReturnHyperStat() {
         runBlocking {
-            runCatching {
-                val ocid = mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME).id
-                mapleCharacterApi.getCharacterHyperStat(
-                    ocid = ocid,
-                    date = "2024-01-22"
-                )
-            }.onSuccess { characterHyperStat ->
-                if(characterHyperStat.jobName == "엔젤릭버스터") {
-                    println(characterHyperStat.toString())
-                    assert(true)
-                }else {
-                    println(characterHyperStat.toString())
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
-            }
+            // Given
+            val ocid = mapleOcidApi.getOcid(characterName = CHARACTER_NAME).id
+
+            // When
+            val hyperStat = mapleCharacterApi.getCharacterHyperStat(ocid = ocid, date = INQUIRY_DATE)
+
+            // Then
+            Assert.assertEquals(hyperStat.jobName, "엔젤릭버스터")
         }
     }
 
-    @Test
-    fun `get hyper stat model test`() {
-        runBlocking {
-            runCatching {
-                val ocid = mapleOcidApi.getOcid(characterName = TestParameter.CHARACTER_NAME).id
-                mapleCharacterApi.getCharacterHyperStat(
-                    ocid = ocid,
-                    date = "2024-01-22"
-                ).toModel()
-            }.onSuccess { characterHyperStat ->
-                if(characterHyperStat.jobName == "엔젤릭버스터") {
-                    println(characterHyperStat.toString())
-                    assert(true)
-                }else {
-                    println(characterHyperStat.toString())
-                    assert(false)
-                }
-            }.onFailure {
-                println(it.message)
-                assert(false)
-            }
-        }
-    }
 }
