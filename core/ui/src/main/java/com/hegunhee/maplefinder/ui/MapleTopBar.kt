3e47e6c9c4a0ab1ce701e9d7b898974e3a3cd47c
이건희 <leech9876@naver.com>
2024-12-26 14:07:24 +0900

    #48 - feat : MapleTopBar preview 작성

--- core/ui/src/main/java/com/hegunhee/maplefinder/ui/MapleTopBar.kt
+++ core/ui/src/main/java/com/hegunhee/maplefinder/ui/MapleTopBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,5 +23,14 @@ fun MapleTopBar(
                 Icon(imageVector = Icons.Default.Menu, contentDescription = null)
             }
         }
+    )
+}
+
+@Preview
+@Composable
+private fun MapleTopBarPreview() {
+    MapleTopBar(
+        title = "타이틀입니다.",
+        onNavigationIconClick = {},
     )
 }
