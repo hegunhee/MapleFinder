ef88b9f21a26bf19c0bc9bad6fdd5d490d572de5
이건희 <leech9876@naver.com>
2024-12-26 16:38:46 +0900

    #50 - feat : ItemNavGraph를 navigation 패키지 안으로 이동, route 컨벤션 통일
    컨벤션에 따라서 navigation 패키지를 만들고 그 안에 NavGraph를 넣었습니다.
    
    ROUTE의 경우 object가 아닌 그냥 상수로 만들었습니다.

--- feature/item/src/main/java/com/hegunhee/maplefinder/item/navigation/ItemNavGraph.kt
+++ feature/item/src/main/java/com/hegunhee/maplefinder/item/navigation/ItemNavGraph.kt
@@ -1,4 +1,4 @@
-package com.hegunhee.maplefinder.item
+package com.hegunhee.maplefinder.item.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -9,12 +9,22 @@ import com.hegunhee.maplefinder.item.detail.ItemDetailScreenRoot
 import com.hegunhee.maplefinder.item.list.ItemListScreenRoot
 import com.hegunhee.maplefinder.item.search.ItemSearchScreenRoot
 
+const val SEARCH_ROUTE = "Search_Item"
+
+fun detailRoute(ocid: String) : String {
+    return "${SEARCH_ROUTE}/${ocid}"
+}
+
+fun detailListRoute(ocid : String,slotName : String) : String {
+    return "${SEARCH_ROUTE}/$ocid/$slotName"
+}
+
 fun NavController.navigateItemDetail(ocid : String) {
-    navigate(ItemNavGraph.detailRoute(ocid))
+    navigate(detailRoute(ocid))
 }
 
 fun NavController.navigateItemList(ocid : String,slotName: String) {
-    navigate(ItemNavGraph.detailListRoute(ocid,slotName))
+    navigate(detailListRoute(ocid, slotName))
 }
 fun NavGraphBuilder.itemNavGraph(
     onNavigationIconClick : () -> Unit,
@@ -22,14 +32,14 @@ fun NavGraphBuilder.itemNavGraph(
     onItemListButtonClick : (String,String) -> Unit,
     onItemDetailButtonClick : (String) -> Unit
 ) {
-    composable(route = ItemNavGraph.searchRoute) {
+    composable(route = SEARCH_ROUTE) {
         ItemSearchScreenRoot(
             onNavigationIconClick = onNavigationIconClick,
             onSearchCharacterItemClick = onItemDetailButtonClick
         )
     }
 
-    composable(route = ItemNavGraph.detailRoute("{ocid}"),
+    composable(route = detailRoute("{ocid}"),
         arguments  = listOf(
             navArgument("ocid") {
                 type = NavType.StringType
@@ -42,7 +52,7 @@ fun NavGraphBuilder.itemNavGraph(
             onItemListButtonClick = onItemListButtonClick
         )
     }
-    composable(route = ItemNavGraph.detailListRoute("{ocid}","{slotName}"),
+    composable(route = detailListRoute("{ocid}", "{slotName}"),
         arguments = listOf(
             navArgument("ocid") {
                 type = NavType.StringType
@@ -55,19 +65,5 @@ fun NavGraphBuilder.itemNavGraph(
             slot = slotName,
             popBackStack = onPopBackStack
         )
-    }
-}
-
-object ItemNavGraph {
-    const val searchRoute = "Search"
-
-    private const val listRoute = "List"
-
-    fun detailRoute(ocid : String) : String {
-        return "$searchRoute/$ocid"
-    }
-
-    fun detailListRoute(ocid : String,slotName : String) : String {
-        return "$listRoute/$ocid/$slotName"
     }
 }
