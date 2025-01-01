418015213b049674a480d42fd84f315ef37630e6
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-01-01 23:38:20 +0900

    무릉도장 화면 날짜 정보 삭제 (#64)
    * #63 - test : 무릉도장 최고기록의 이전날짜의 무릉도장 기록 조회시 에러 발생하는 테스트케이스 추가
    
    * #63 - feat : 무릉도장 검색화면에서 날짜 선택이 불가능하도록 설정
    
    어느 시점을 조회해도 가장 최고기록만 조회되므로 날짜 선택이 불필요함
    (조회 날짜는 최신날짜로 설정)

--- core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterDojangUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplefinder/data/api/character/GetCharacterDojangUnitTest.kt
@@ -9,9 +9,13 @@ import com.hegunhee.maplefinder.data.di.ApiModule.provideJson
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleApi
 import com.hegunhee.maplefinder.data.di.ApiModule.provideMapleOcidApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Test
+import java.time.LocalDate
 
 class GetCharacterDojangUnitTest {
 
@@ -41,4 +45,22 @@ class GetCharacterDojangUnitTest {
         }
     }
 
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun givenBeforeBestRecordDate_whenGetDojangInfo_thenReturnException() {
+        runBlocking {
+            // Given
+            val ocid = mapleOcidApi.getOcid(characterName = CHARACTER_NAME).id
+            val bestDojangRecord = mapleCharacterApi.getCharacterDojang(ocid = ocid, date = INQUIRY_DATE)
+
+            val result = runCatching { mapleCharacterApi.getCharacterDojang(ocid = ocid, bestDojangRecord.dojangRecordDate.toLocalDateString())  }
+
+            // When & Then
+            assertThrows(MissingFieldException::class.java) { result.getOrThrow() }
+        }
+    }
+
+    private fun String.toLocalDateString(): String {
+        return LocalDate.parse(this.split("T")[0]).toString()
+    }
 }
