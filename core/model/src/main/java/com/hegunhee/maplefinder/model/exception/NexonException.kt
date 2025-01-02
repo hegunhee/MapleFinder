f5eccbc08d20e64794d0a25e8f4eb32d89300948
이건희 <leech9876@naver.com>
2025-01-02 15:45:03 +0900

    #65 - style : sealed class 끝줄 공백으로 변경

--- core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
+++ core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
@@ -11,4 +11,5 @@ sealed class NexonApiException(val code : Int, override val message : String) : 
     object InvailedParameterException : NexonApiException(code = 400, message = "캐릭터 이름이 유효하지 않습니다")
 
     object UnretrivableException : NexonApiException(code = 404, message = "존재하지만 조회가 불가능합니다.")
+    
 }
