ee4a1329f6e9c3578a153e90957c00735493b5ec
이건희 <leech9876@naver.com>
2025-01-08 10:50:16 +0900

    #80 - feat : 아이템의 height값을 아이템 이미지 값과 동일하게 설정

--- core/ui/src/main/java/com/hegunhee/maplefinder/ui/surface/item/CashItemSurface.kt
+++ core/ui/src/main/java/com/hegunhee/maplefinder/ui/surface/item/CashItemSurface.kt
@@ -1,6 +1,7 @@
 package com.hegunhee.maplefinder.ui.surface.item
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,6 +9,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -15,6 +19,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -84,7 +90,7 @@ private fun CashItem(
 ) {
     Card(
         modifier = Modifier
-            .size(width = 200.dp, height = 100.dp)
+            .width(200.dp)
             .border(2.dp, Color.Black)
             .padding(5.dp),
         shape = RectangleShape
@@ -103,9 +109,8 @@ private fun CashItem(
             )
             Column {
                 Text(item.slot)
-                Text(item.name)
+                Text(item.name, fontSize = 13.sp)
             }
-
         }
     }
 }
