c354ebf7fe09a7ee5042559f9c77ede5b0c36261
이건희 <leech9876@naver.com>
2025-01-10 16:56:36 +0900

    #82 -feat : 알 수 없는 네트워크 상태의 커스텀 에러도 생성

--- core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
+++ core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
@@ -12,6 +12,8 @@ sealed class NexonApiException(val code : Int, override val message : String) : 
 
     object InvailedApiKeyException : NexonApiException(code = 400, message = "api key가 유효하지 않습니다.")
 
+    object IllegalStateException : NexonApiException(code = 400, message = "알수없는 네트워크 에러가 발생했습니다.")
+
     object UnretrivableException : NexonApiException(code = 404, message = "존재하지만 조회가 불가능합니다.")
     
 }
