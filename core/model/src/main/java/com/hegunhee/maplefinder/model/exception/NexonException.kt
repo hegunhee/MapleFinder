d3e238f8e9effb8e21708dfd1bd40f78931b38e9
이건희 <leech9876@naver.com>
2025-01-10 16:54:40 +0900

    #82 - feat : api key 누락에 해당하는 CustomException 생성
    캐릭터 이름이 이상한것과 api key 누락은 분리해야되기때문에 분리함

--- core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
+++ core/model/src/main/java/com/hegunhee/maplefinder/model/exception/NexonException.kt
@@ -10,6 +10,8 @@ sealed class NexonApiException(val code : Int, override val message : String) : 
 
     object InvailedParameterException : NexonApiException(code = 400, message = "캐릭터 이름이 유효하지 않습니다")
 
+    object InvailedApiKeyException : NexonApiException(code = 400, message = "api key가 유효하지 않습니다.")
+
     object UnretrivableException : NexonApiException(code = 404, message = "존재하지만 조회가 불가능합니다.")
     
 }
