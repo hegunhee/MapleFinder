18a2c3638e7206e80edb296598a71700e22b760c
이건희 <leech9876@naver.com>
2025-01-10 17:30:22 +0900

    #82 - feat : 400번의 HttpException이 들어올 때 응답을 파싱해서 적절한 예외로 변환
    테스트케이스를 만드려했으나
    직접 response를 만들거나 헤더를 조작하는방법이 없어보여서 테스트케이스는 없음

--- core/data/src/main/java/com/hegunhee/maplefinder/data/api/createResult.kt
+++ core/data/src/main/java/com/hegunhee/maplefinder/data/api/createResult.kt
@@ -1,8 +1,11 @@
 package com.hegunhee.maplefinder.data.api
 
+import com.hegunhee.maplefinder.data.api.model.error.ErrorDetail
+import com.hegunhee.maplefinder.data.api.model.error.ErrorResponse
 import com.hegunhee.maplefinder.model.exception.NexonApiException
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.MissingFieldException
+import kotlinx.serialization.json.Json
 import retrofit2.HttpException
 
 @OptIn(ExperimentalSerializationApi::class)
@@ -14,7 +17,10 @@ suspend fun <T, R> T.createResult(call : suspend T.() -> R) : Result<R> {
             500 -> { NexonApiException.ServerException }
             429 -> { NexonApiException.TooManyRequestException }
             403 -> { NexonApiException.ForbiddenException }
-            400 -> { NexonApiException.InvailedParameterException }
+            400 -> {
+                val errorResponse = json.decodeFromString<ErrorResponse>(httpE.getJsonErrorBodyString())
+                create400CustomException(errorResponse.errorDetail)
+            }
             else -> { httpE }
         })
     } catch (missingFieldE: MissingFieldException) {
@@ -24,4 +30,28 @@ suspend fun <T, R> T.createResult(call : suspend T.() -> R) : Result<R> {
     } catch (e: Throwable) {
         Result.failure(e)
     }
+}
+
+private fun HttpException.getJsonErrorBodyString(): String{
+    return response()?.errorBody()?.string() ?: ""
+}
+
+private fun create400CustomException(errorDetail: ErrorDetail): NexonApiException {
+    return when (errorDetail.name) {
+        "OPENAPI00005" -> {
+            NexonApiException.InvailedApiKeyException
+        }
+        "OPENAPI00004" -> {
+            NexonApiException.InvailedParameterException
+        }
+        else -> {
+            NexonApiException.IllegalStateException
+        }
+    }
+}
+
+private val json = Json {
+    ignoreUnknownKeys = true
+    isLenient = true
+    coerceInputValues = true
 }
